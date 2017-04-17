c0f9269cb53279526c50a15348cf3d4807c8a6ac
ThorSiv <thorsivodin@gmail.com>
2017-04-17 17:53:49 +0800

    delete ip

--- delete.py
+++ delete.py
@@ -2,7 +2,7 @@
 import json,sys,re,urllib2
 
 def http_post_gethostid(hostname):
-        requrl="http://101.200.210.206/lisa/api_jsonrpc.php"
+        requrl="http:///lisa/api_jsonrpc.php"
         values={
             "jsonrpc": "2.0",
             "method": "host.get",
@@ -12,7 +12,7 @@ def http_post_gethostid(hostname):
                         "host":[hostname]
                 }
             },
-            "auth": "a0a261a8f2801d774df794a426cdc237",
+            "auth": "",
             "id": 1
             #"auth": "null"
         }
@@ -29,12 +29,12 @@ hostid = decode["result"][0]["hostid"]
 
 
 def http_post_delete(idofhost):
-        requrl="http://101.200.210.206/lisa/api_jsonrpc.php"
+        requrl="http:///lisa/api_jsonrpc.php"
         values={
             "jsonrpc": "2.0",
             "method": "host.delete",
             "params": [idofhost],
-	    "auth": "a0a261a8f2801d774df794a426cdc237",
+	    "auth": "",
             "id": 1
             #"auth": "null"
         }
